df28b96c5f3d26e190e9a5f9ebc5941204804bba
Coman Patricia <comanpatricia27@gmail.com>
2022-03-03 16:54:47 +0200

    UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController
+{
+    /**
+     * @Route(path="/users")
+     */
+
+    public function getAllAction(Request $request): Response
+    {
+        return new Response("Hello Im Patricia", Response::HTTP_OK, []);
+    }
+}
