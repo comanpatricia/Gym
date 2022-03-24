bad26b40a0ad9aad26b3abb6666f73e80267a504
Coman Patricia <comanpatricia27@gmail.com>
2022-03-24 15:47:41 +0200

    Work on tests

---
+++ tests/Controller/ProgrammeControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class ProgrammeControllerTest extends TestCase
+{
+//    private ProgrammeController $programmeController;
+//
+//    protected function setUp(): void
+//    {
+//        $this->programmeController = new ProgrammeController();
+//    }
+//
+//    public function testListFilteredProgrammes(): void
+//    {
+//        $request = Request::create('/test');
+//        $result = $this->programmeController->listFilteredProgrammes($request);
+//
+//        self::assertTrue($result);
+//    }
+//
+//    public function
+}
