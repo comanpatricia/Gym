8c03190c6d59f4d604fec92cb4fe006044a269e3
Coman Patricia <comanpatricia27@gmail.com>
2022-03-25 09:05:05 +0200

    Created classes for tests, not finished

--- tests/Controller/ProgrammeControllerTest.php
+++ tests/Controller/ProgrammeControllerTest.php
@@ -6,20 +6,5 @@ use PHPUnit\Framework\TestCase;
 
 class ProgrammeControllerTest extends TestCase
 {
-//    private ProgrammeController $programmeController;
-//
-//    protected function setUp(): void
-//    {
-//        $this->programmeController = new ProgrammeController();
-//    }
-//
-//    public function testListFilteredProgrammes(): void
-//    {
-//        $request = Request::create('/test');
-//        $result = $this->programmeController->listFilteredProgrammes($request);
-//
-//        self::assertTrue($result);
-//    }
-//
-//    public function
+
 }
