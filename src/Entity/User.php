edb96ee5577689b2a68f342485ab876b811c7034
Coman Patricia <comanpatricia27@gmail.com>
2022-04-06 14:29:44 +0300

    Verified and refactored reset password function

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Controller\Dto\UserDto;
 use App\Repository\UserRepository;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -98,6 +99,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="datetime", nullable=true)
      */
     private ?\DateTime $tokenResetCreatedAt;
+
+    /**
+    * @ORM\Column(name="deletedAt", type="datetime", nullable=true)
+    */
+    private ?DateTime $deletedAt;
 
     public function __construct()
     {
