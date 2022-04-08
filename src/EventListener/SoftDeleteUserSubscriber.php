2cf86545f5661ad6b6ac2901d1e77bbc1f585f4c
Coman Patricia <72436614+comanpatricia@users.noreply.github.com>
2022-04-08 17:14:10 +0300

    Implemented a subscriber to soft delete an user (#29)

---
+++ src/EventListener/SoftDeleteUserSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use App\Repository\ProgrammeRepository;
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Gedmo\SoftDeleteable\SoftDeleteableListener;
+
+class SoftDeleteUserSubscriber implements EventSubscriberInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    private ProgrammeRepository $programmeRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, ProgrammeRepository $programmeRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->programmeRepository = $programmeRepository;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [SoftDeleteableListener::POST_SOFT_DELETE];
+    }
+
+    public function postSoftDelete(LifecycleEventArgs $lifecycleEventArgs): void
+    {
+        $user = $lifecycleEventArgs->getObject();
+        $programmes = $this->programmeRepository->findBy(['trainer' => $user]);
+
+        if (!$user instanceof User || !in_array('ROLE_TRAINER', $user->getRoles(), true)) {
+            return;
+        }
+
+        foreach ($programmes as $programme) {
+            $programme->setTrainer(null);
+            $this->entityManager->persist($programme);
+        }
+
+        $this->entityManager->flush();
+    }
+}
