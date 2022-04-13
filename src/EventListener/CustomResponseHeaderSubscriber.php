78f738e0f3a04167b9f5d24546f1907d38cabaa6
Coman Patricia <comanpatricia27@gmail.com>
2022-04-13 14:48:47 +0300

    Working on custom response header

---
+++ src/EventListener/CustomResponseHeaderSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class CustomResponseHeaderSubscriber implements EventSubscriberInterface
+{
+    private string $apiVersion;
+
+    public function __construct(string $apiVersion)
+    {
+        $this->apiVersion = $apiVersion;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [ResponseEvent::class => 'addApiVersionHeader'];
+    }
+
+    public function addApiVersionHeader(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest()->attributes->get('_route');
+
+        if ($request === null || \strpos('api', $request) === false) {
+            var_dump($request);
+            return;
+        }
+
+        $response = $event->getResponse();
+        $response->headers->set('X-Api-Version', $this->apiVersion);
+    }
+}
