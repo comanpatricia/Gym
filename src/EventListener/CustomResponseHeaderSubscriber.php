0e31617dc398c7e86ec78d0f7181bd7b71ae2f0c
Coman Patricia <comanpatricia27@gmail.com>
2022-04-13 15:59:51 +0300

    Implementesd custom response header function

--- src/EventListener/CustomResponseHeaderSubscriber.php
+++ src/EventListener/CustomResponseHeaderSubscriber.php
@@ -24,13 +24,11 @@ class CustomResponseHeaderSubscriber implements EventSubscriberInterface
         if (!$event->isMainRequest()) {
             return;
         }
-
         $request = $event->getRequest()->attributes->get('_route');
 
         if ($request === null || \strpos($request, 'api_') === false) {
             return;
         }
-
         $response = $event->getResponse();
         $response->headers->set('X-Api-Version', $this->apiVersion);
     }
